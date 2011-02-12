136e1ee9e8b217fc930b3eea53a70e01ddb07fb0
Ryan Wolf <rwolf@borderstylo.com>
2011-02-12 12:29:24 -0800

    solved 22

---
+++ projecteuler/022/022.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+
+names = File.open("names.txt").first
+names.chomp
+names = names.gsub(/["\s]/, '').split(",")
+
+def score (name, index)
+  s = 0
+  for i in (0...name.size)
+    c = name[i] - 64
+    s += c
+  end
+  s * (index + 1)
+end
+
+total = 0
+names.sort.each_with_index {|name, i|
+  total += score name, i
+}
+
+puts total
